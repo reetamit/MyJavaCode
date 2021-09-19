8d13d9778ff6a6e0324b982688b5cc4a165cd46a
Reetam Biswas <reetamit@gmail.com>
2021-09-19 19:11:03 -0400

    master

--- MyRunnable.java
+++
@@ -1,6 +0,0 @@
-
-class MyRunnableProject {
-    public static void main(String[] args) {
-        System.out.println("Simple a text.");
-    }
-}
---
+++ com/java/mypackage/NewClass.java
@@ -0,0 +1,14 @@
+package com.java.mypackage;
+
+public class NewClass {
+    private int Id;
+
+    public NewClass(int id)
+    {
+        Id=id;
+    }
+
+    public int getId() {
+        return Id;
+    } 
+}
