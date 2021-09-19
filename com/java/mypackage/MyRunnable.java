a6e687b94f80148dd5c3eb5d1c64fba34d42baba
Reetam Biswas <reetamit@gmail.com>
2021-09-19 19:09:37 -0400

    Commit

---
+++ com/java/mypackage/MyRunnable.java
@@ -0,0 +1,15 @@
+package com.java.mypackage;
+
+class MyRunnableProject {
+    public static void main(String[] args) {
+
+        var obj = new NewClass(12);
+        System.out.println(obj.getId());
+
+        String a = "Test";
+        String b = "Test";
+        String c = new String("test");
+
+        System.out.println(c.toString());
+    }
+}
