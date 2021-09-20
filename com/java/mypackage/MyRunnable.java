3cb3bbbceefb92cae356dca0ac60ca45604b49a8
Reetam Biswas <reetamit@gmail.com>
2021-09-20 01:22:05 -0400

    PeriodicUpdate

--- com/java/mypackage/MyRunnable.java
+++ com/java/mypackage/MyRunnable.java
@@ -1,15 +1,56 @@
 package com.java.mypackage;
 
+import java.io.*;
+import java.nio.charset.Charset;
+
 class MyRunnableProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        var obj = new NewClass(12);
-        System.out.println(obj.getId());
+       /* var obj = new NewClass(12);
+        String[] random_array = {"1234","ABCD","Hati","Bang","ChatarMata"};
+        for (String r_array : random_array) {
+            System.out.println(r_array);
+        }
+        for(int i=0;i<random_array.length;i++)
+        {
+                System.out.println(random_array[i]);
+        }*/
 
-        String a = "Test";
-        String b = "Test";
-        String c = new String("test");
+    //Java style printing
+    //System.out.println(obj.getId());
+    //C++ style printing
+    /*  System.out.printf("Obj ID is : %d",obj.getId()).println();
+        FileReader file = new FileReader("E:\\WorkSpace\\Dummy_letter.txt");
+        BufferedReader fileInput = new BufferedReader(file);
+          
+        // Print first 3 lines of file "C:\test\a.txt"
+        for (int counter = 0; counter < 3; counter++) 
+            System.out.println(fileInput.readLine());
+          
+        fileInput.close();*/
 
-        System.out.println(c.toString());
+       //String s1 = "Test B";
+      // String s2 = "Test A";
+
+      // int out = s1.compareTo(s2);
+       //System.out.println(s2.hashCode());
+
+      /* int arr[][] = { {2,7,9},{3,6,1},{7,4,2} };
+  
+       // printing 2D array
+       for (int i=0; i< 3 ; i++)
+       {
+           for (int j=0; j < 3 ; j++)
+               System.out.print(arr[i][j] + " ");
+ 
+           System.out.println();
+       }*/
+
+       /*MySingleton a = MySingleton.getInstance();
+       MySingleton b = MySingleton.getInstance();
+       a.x = a.x + 10;
+       System.out.println("Value of a.x = " + a.x);
+       System.out.println("Value of b.x = " + b.x);*/
+       
     }
 }
---
+++ com/java/mypackage/MySingleton.java
@@ -0,0 +1,19 @@
+package com.java.mypackage;
+
+public class MySingleton {
+
+    static MySingleton instance = null;
+    public int x = 10;
+    
+    // private constructor can't be accessed outside the class
+    private MySingleton() {  }
+   
+    // Factory method to provide the users with instances
+    static public MySingleton getInstance()
+    {
+        if (instance == null)        
+             instance = new MySingleton();
+   
+        return instance;
+    } 
+}
